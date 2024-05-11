da17c14c36019434efcc4b68b4e48e0dbf3cc309
TheTaurunti <153017348+TheTaurunti@users.noreply.github.com>
2024-05-11 13:32:38 -0700

    gave planner unique name

--- prototypes/planner.lua
+++ prototypes/planner.lua
@@ -15,23 +15,23 @@ data:extend({
     stack_size = 1,
     stackable = false,
 
-    selection_mode = {"buildable-type"},
-    selection_color = {r = 0.7, g = 0.7, b = 0},
+    selection_mode = { "buildable-type" },
+    selection_color = { r = 0.7, g = 0.7, b = 0 },
     selection_cursor_box_type = "entity",
-    
+
     -- Not actually using alt mode, so give it same settings?
-    alt_selection_mode = {"buildable-type"},
-    alt_selection_color = {r = 0.7, g = 0.7, b = 0},
+    alt_selection_mode = { "buildable-type" },
+    alt_selection_color = { r = 0.7, g = 0.7, b = 0 },
     alt_selection_cursor_box_type = "entity",
 
-    -- 
-    reverse_selection_mode = {"buildable-type"},
-    reverse_selection_color = {r = 0.9, g = 0.5, b = 0.1},
+    --
+    reverse_selection_mode = { "buildable-type" },
+    reverse_selection_color = { r = 0.9, g = 0.5, b = 0.1 },
     reverse_selection_cursor_box_type = "entity"
   },
   {
     type = "shortcut",
-    name = "ore-patch-organizer",
+    name = "bcplus-planner",
     icon = {
       filename = "__BigContainersUPSPlus__/prototypes/tool.png",
       size = 64,
